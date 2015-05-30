abca46b9980e29f0242804bcd74c85e0684212d4
Kyle Rush <kylerrush@gmail.com>
2015-05-30 15:28:45 -0700

    Add name property for basename sans extension.

--- test/index.js
+++ test/index.js
@@ -18,6 +18,7 @@ describe('Metalsmith Paths', function () {
       files['path/to/file.ext'].should.have.property('dirname').and.equal('path/to')
       files['path/to/file.ext'].should.have.property('extname').and.equal('.ext')
       files['path/to/file.ext'].should.have.property('basename').and.equal('file.ext')
+      files['path/to/file.ext'].should.have.property('name').and.equal('file')
       files['path/to/file.ext'].should.have.property('path').and.equal('/path/to/')
 
       done()
