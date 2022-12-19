63b4b443b2466bc05fe029adc12dce6575670f7b
Ahmad Nassri <ahmad@ahmadnassri.com>
2022-12-19 17:24:48 +0000

    ci: adopt template-node-lib

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
+const { platform } = require('os')
 const { parse, basename } = require('path')
 const { debuglog } = require('util')
 
-const os = require('os')
-const path = require('path')
-const util = require('util')
-
-const debug = util.debuglog('metalsmith-paths')
+const debug = debuglog('metalsmith-paths')
 const defaults = {
   property: 'path',
   directoryIndex: false,
@@ -24,33 +21,18 @@ module.exports = function (options) {
     setImmediate(done)
 
     Object.keys(files).forEach((file) => {
-      debug('process file: %s', file)
+      let filename = file
 
-      // Assign to new file, to not alter the object key
-      let toParse = file
-
-      if (options.parseWindows && os.platform() === 'win32') {
+      // manage windows paths
+      if (options.parseWindows && platform() === 'win32') {
         const driveRegex = /^[a-z]:\\{0,2}/i
-        toParse = file.replace(driveRegex, '/').replace(/\\{1,2}/g, '/')
-        debug(toParse)
+        filename = file.replace(driveRegex, '/').replace(/\\{1,2}/g, '/')
+        debug(filename)
       }
 
-      if (path.parse) {
-        debug('[node >= 0.11.15] using path.parse')
+      debug('process file: %s (%s)', file, filename)
 
-        files[file][options.property] = path.parse(toParse)
-      } else {
-        // add file path info
-        let extname = path.extname(toParse)
-
-        files[file][options.property] = {
-          base: path.basename(toParse),
-          dir: path.dirname(toParse).split(path.sep).join('/'),
-          ext: extname,
-          name: path.basename(toParse, extname)
-        }
-        debug(files[file][options.property])
-      }
+      files[file][options.property] = parse(filename)
 
       // In some versions of node/path, 'dir' at root may be either '.' or empty
       // Normalize this property to be empty
@@ -67,13 +49,12 @@ module.exports = function (options) {
 
       const dhref = href
 
-      if (!options.directoryIndex || path.basename(toParse) !== options.directoryIndex) {
-        href += path.basename(toParse)
+      if (!options.directoryIndex || basename(filename) !== options.directoryIndex) {
+        href += basename(filename)
       }
 
       files[file][options.property].href = href
       files[file][options.property].dhref = dhref
     })
-    debug(files)
   }
 }
